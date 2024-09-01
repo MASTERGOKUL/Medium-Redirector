8b1faed8eddca07e79d8af432aeae970d51df9db
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2024-09-01 13:53:55 +0530

    init up

---
+++ medium_redirector/scripts.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("readmedium").addEventListener("click", function () {
+    var site = "readmedium";
+    redirect(site);
+  });
+
+  document.getElementById("freedium").addEventListener("click", function () {
+    var site = "freedium";
+    redirect(site);
+  });
+});
+
+function redirect(site) {
+  (async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+    });
+
+    var medium_full_link = tab.url; // normal window.location.href will not work
+   
+    var medium_link = medium_full_link.split("//");
+    var medium_link = medium_link[1].split("/");
+    if (site === "readmedium") {
+      medium_link[0] = "readmedium.com";
+      chrome.tabs.create({ url: "https://" + medium_link.join("/") }, function (newTab) {
+       
+        chrome.tabs.move(newTab.id, { index: tab.index + 1 }); // Move the new tab next to the current tab
+      });
+      // chrome.tabs.update(tab.id, { url: "https://" + medium_link.join("/")}); // normal window.location.href will not work
+    } else if (site === "freedium") {
+        medium_link[0] = "freedium.cfd";
+        chrome.tabs.create({ url: "https://" + medium_link.join("/") }, function (newTab) {
+          chrome.tabs.move(newTab.id, { index: tab.index + 1 });
+        });
+    }
+  })();
+}
