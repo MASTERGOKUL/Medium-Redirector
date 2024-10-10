5f927adfa218fafed5b9ca27fbe3ab54f0814187
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2024-10-10 18:11:39 +0530

    added for firefox browser also

--- medium_redirector/scripts.js
+++ medium_redirector/scripts.js
@@ -12,7 +12,24 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function redirect(site) {
+function redirect(site){
+  var userAgent = navigator.userAgent;
+    if (userAgent.indexOf("Edg") > -1) {
+      return "Microsoft Edge";
+  } else if (userAgent.indexOf("Chrome") > -1) {
+    redirect_chrome(site);
+  } else if (userAgent.indexOf("Firefox") > -1) {
+    redirect_firefox(site);
+  } else if (userAgent.indexOf("Safari") > -1) {
+      return "Safari";
+  } else if (userAgent.indexOf("Opera") > -1) {
+      return "Opera";
+  } else if (userAgent.indexOf("Trident") > -1 || userAgent.indexOf("MSIE") > -1) {
+      return "Internet Explorer";
+  }
+}
+
+function redirect_chrome(site) {
   (async () => {
     const [tab] = await chrome.tabs.query({
       active: true,
@@ -39,6 +56,40 @@ function redirect(site) {
     else if (site === "github") {
       chrome.tabs.create({ url: "https://github.com/mastergokul" }, function (newTab) {
         chrome.tabs.move(newTab.id, { index: tab.index + 1 });
+      }); 
+    }
+  })();
+}
+
+
+
+function redirect_firefox(site) {
+  (async () => {
+    const [tab] = await browser.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+    });
+
+    var medium_full_link = tab.url; // normal window.location.href will not work
+   
+    var medium_link = medium_full_link.split("//");
+    var medium_link = medium_link[1].split("/");
+    if (site === "readmedium") {
+      medium_link[0] = "readmedium.com";
+      browser.tabs.create({ url: "https://" + medium_link.join("/") }, function (newTab) {
+       
+        browser.tabs.move(newTab.id, { index: tab.index + 1 }); // Move the new tab next to the current tab
+      });
+      // browser.tabs.update(tab.id, { url: "https://" + medium_link.join("/")}); // normal window.location.href will not work
+    } else if (site === "freedium") {
+        medium_link[0] = "freedium.cfd";
+        browser.tabs.create({ url: "https://" + medium_link.join("/") }, function (newTab) {
+          browser.tabs.move(newTab.id, { index: tab.index + 1 });
+        });
+    }
+    else if (site === "github") {
+      browser.tabs.create({ url: "https://github.com/mastergokul" }, function (newTab) {
+        browser.tabs.move(newTab.id, { index: tab.index + 1 });
       }); 
     }
   })();
